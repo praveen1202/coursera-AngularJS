8e038afc577253faa81080c164c4f0f5a4e7cde7
Praveen Kumar S <spraveeniit@gmail.com>
2020-05-27 16:41:09 +0530

    Update app.js

--- modsoln1/app.js
+++ modsoln1/app.js
@@ -1,4 +1,4 @@
-(function(){
+/*(function(){
     'use strict';
     angular.module('LunchCheck',[])
     .controller('LunchCheckController',LunchCheckController);
@@ -15,3 +15,35 @@
         }
     }
 })();
+*/
+(function () {
+
+    angular.module('LunchCheck', [])
+      .controller('LunchCheckController', LunchCheckController);
+  
+    LunchCheckController.$inject = ['$scope'];
+  
+    function LunchCheckController($scope) {
+      $scope.message = "";
+  
+      $scope.displayMessage = function() {
+        var items;
+  
+        if ($scope.input) {
+          $scope.input = $scope.input.trim();
+          items = $scope.input.split(",");
+          items = items.filter(function(item) {
+            return item.length > 0;
+          });
+          if (items.length <= 3) {
+            $scope.message = "Enjoy!";
+          } else {
+            $scope.message = "Too much!";
+          }
+        } else {
+          $scope.message = "Please enter data first";
+        }
+      }
+    }
+  
+  })();
